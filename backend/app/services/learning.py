055dc3c381be84e36726c51ff8c4ef038589acbc
Yan <2920241726@qq.com>
2025-02-24 16:38:56 +0800

    0224初始化CF项目版本

---
+++ backend/app/services/learning.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+from markupsafe import escape
+
+app = Flask(__name__)
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {escape(name)}!'
+
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    username = request.form.get('username')
+    return f'Hello, {username}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
